a903a918ba94d1872e41140336acf99bc172f37b
joseI <joseimbacuan705@gmail.com>
2022-04-15 14:31:20 -0500

    controladores

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,6 @@ export * from './ping.controller';
 export * from './usuario-rol.controller';
 export * from './rol-usuario.controller';
 export * from './rol-permiso.controller';
+export * from './rol.controller';
+export * from './usuario.controller';
+export * from './permiso.controller';
